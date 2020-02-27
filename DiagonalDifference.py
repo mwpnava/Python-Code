fec6efcd6e92685c2fb26686fb746975bf51f9f5
Wendy Navarrete <navarrete.wen@gmail.com>
2020-02-27 15:16:09 -0800

    Diagonal Difference (Easy)

--- DiagonalDifference.py
+++ DiagonalDifference.py
@@ -7,8 +7,11 @@ def diagonalDifference(arr):
     Return: An integer (The result of sums of matrix's diagonals)
     '''
 
-    sum1,sum2 = 0,0
-    i2 = len(arr) -1
+    #Edge case
+    if len(arr) == 0:
+        return 0
+
+    sum1,sum2,i2 = 0,0,len(arr) -1
 
     for i in range(len(arr)):
         sum1 += arr[i][i]
@@ -19,7 +22,6 @@ def diagonalDifference(arr):
 
 
 #Testing function
-
 X = [[11,2,4],[4,5,6],[10,8,-12]]
 result = diagonalDifference(X)
 print(result)
